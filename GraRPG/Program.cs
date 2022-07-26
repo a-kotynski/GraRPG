29f3eb3135208c57ca09efd50b4ee9a7de3fbdd2
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-26 15:01:59 +0200

    returns

--- GraRPG/Program.cs
+++ GraRPG/Program.cs
@@ -1,80 +1,94 @@
-﻿using GameRPG;
+﻿// See https://aka.ms/new-console-template for more information
+using GameRPG;
 using System;
+
 Console.WriteLine("Hello, World!");
+
 Fellowship party = new Fellowship("Drużyna Pierścienia");
 party.AddHero(new Warrior());
-party.AddHero(new Warrior("Andrzej Dupa", 0.5f, 3));
+party.AddHero(new Warrior("Andrzej Dupa", 0.5m, 3));
 party.AddHero(new Wizard());
-party.AddHero(new Wizard("Marcin Gortat", 1.0f, 6, 12));
-party.Heroes[1].HealthChange(0.47f);
+party.AddHero(new Wizard("Marcin Gortat", 1.0m, 6, 12));
+
+Console.WriteLine(party.Heroes[1].HealthChange(-0.47m));
 Console.WriteLine(party.ToString());
+
 namespace GameRPG
 {
     public abstract class Hero
     {
         protected string Name { get; set; }
-        protected float HP { get; set; }
+        protected decimal HP { get; set; }
         protected int Strength { get; set; }
-        internal float HealthChange(float hpChange)
+
+        internal abstract decimal AttackPower();
+
+        internal decimal HealthChange(decimal hpChange)
         {
             if (HP + hpChange < 0)
             {
-                return 0;
+                HP = 0;
             }
-            else if (HP + hpChange > 1.0f)
+            else if (HP + hpChange > 1.0m)
             {
-                return 1.0f;
+                HP = 1.0m;
             }
             else
             {
-                return HP + hpChange;
+                HP += hpChange;
             }
+            return HP;
         }
-        internal abstract float AttackPower();
+
+        public override string ToString()
+        {
+            return $"{Name} {HP} {AttackPower()}";
+        }
     }
+
     public class Wizard : Hero
     {
         private int MP { get; set; }
+
         public Wizard()
         {
             Name = "Xardas";
-            HP = 1.0f;
+            HP = 1.0m;
             Strength = Dice.MultipleRoll(1, 6);
             MP = Dice.MultipleRoll(2, 6);
         }
-        public Wizard(string name, float hp, int strength, int mp)
+        public Wizard(string name, decimal hp, int strength, int mp)
         {
             Name = name;
             HP = hp;
             Strength = strength;
             MP = mp;
         }
-        internal override float AttackPower()
+
+        internal override decimal AttackPower()
         {
             return (MP + Strength) * HP;
         }
-        public override string ToString()
-        {
-            return $"{Name} {HP} {AttackPower()}";
-        }
     }
+
     public class Warrior : Hero
     {
         public Warrior()
         {
             Name = "Geralt";
-            HP = 1.0f;
+            HP = 1.0m;
             Strength = Dice.MultipleRoll(3, 6);
         }
-        public Warrior(string name, float hp, int strength)
+        public Warrior(string name, decimal hp, int strength)
         {
             Name = name;
             HP = hp;
             Strength = strength;
         }
-        internal override float AttackPower()
+
+        internal override decimal AttackPower()
         {
-            if (HP < 0.05f)
+            if (HP < 0.05m)
             {
                 return Strength;
             }
@@ -83,11 +97,8 @@ namespace GameRPG
                 return HP * Strength;
             }
         }
-        public override string ToString()
-        {
-            return $"{Name} {HP} {AttackPower()}";
-        }
     }
+
     abstract class Dice
     {
         public static int MultipleRoll(int rolls, int maxDiceValue)
@@ -95,6 +106,7 @@ namespace GameRPG
             return new Random().Next(rolls, rolls * maxDiceValue + 1);
         }
     }
+
     internal class Fellowship : ICloneable
     {
         public string Name { get; set; }
@@ -103,17 +115,19 @@ namespace GameRPG
         {
             Name = name;
         }
+
         public void AddHero(Hero hero)
         {
             Heroes.Add(hero);
         }
+
         public object Clone()
         {
             return new Fellowship(Name);
         }
-        public float AttackSum()
+        public decimal AttackSum()
         {
-            float sum = 0;
+            decimal sum = 0;
             foreach (Hero hero in Heroes)
             {
                 sum += hero.AttackPower();
@@ -124,7 +138,7 @@ namespace GameRPG
         {
             return $"Group name {Name}" +
                 $"\nAttack sum: {AttackSum()}" +
-                $"\nCharacter list: {Heroes.ToString()}";
+                $"\nCharacter list: {string.Join('\n', Heroes)}";
         }
     }
 }
